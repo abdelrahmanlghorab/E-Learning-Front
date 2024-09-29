65a87a03cb79463fcf9e1829f00c87ace4361d3a
MahmoudTtantawy <tantawym@gmail.com>
2024-09-29 15:33:14 +0300

    Designed sign-in page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignInComponent } from "./Auth/sign-in/sign-in.component";
+import { FooterComponent } from "./Shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SignInComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
