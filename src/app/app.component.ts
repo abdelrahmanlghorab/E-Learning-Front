94fa71e22694776963cf6b83ab9118f1e0a5bdbd
MahmoudTtantawy <tantawym@gmail.com>
2024-09-29 18:17:45 +0300

    Designed TeacherSignUp page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignInComponent } from "./Auth/sign-in/sign-in.component";
 import { StudentSignUPComponent } from "./Auth/Registeration/student-sign-up/student-sign-up.component";
+import { TeacherSignUPComponent } from "./Auth/Registeration/teacher-sign-up/teacher-sign-up.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SignInComponent, StudentSignUPComponent],
+  imports: [RouterOutlet, SignInComponent, StudentSignUPComponent, TeacherSignUPComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
