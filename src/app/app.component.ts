f0b9b382fdaa5934069603aa333ff88859e6b5ea
“Mohamed <mohamedebraheem19246@gmail.com>
2024-09-29 16:32:19 +0300

    teacher && courses && about

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OurTeacherComponent } from './LandingPage/our-teacher/our-teacher.component';
+import { AboutUsComponent } from './LandingPage/about-us/about-us.component';
+import { MostResentCoursesComponent } from './LandingPage/most-resent-courses/most-resent-courses.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,OurTeacherComponent , AboutUsComponent ,MostResentCoursesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
