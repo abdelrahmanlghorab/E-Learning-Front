461e94b5fd3bb9be53e333184eef006c9f92b148
MahmoudTtantawy <tantawym@gmail.com>
2024-09-29 21:14:52 +0300

    Designed ForgetPassword page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { SignInComponent } from "./Auth/sign-in/sign-in.component";
 import { StudentSignUPComponent } from "./Auth/Registeration/student-sign-up/student-sign-up.component";
 import { TeacherSignUPComponent } from "./Auth/Registeration/teacher-sign-up/teacher-sign-up.component";
+import { ForgetPasswordComponent } from "./Auth/forget-password/forget-password.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SignInComponent, StudentSignUPComponent, TeacherSignUPComponent],
+  imports: [RouterOutlet, SignInComponent, StudentSignUPComponent, TeacherSignUPComponent, ForgetPasswordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
