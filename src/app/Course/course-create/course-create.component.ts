e7792110461a1367fb66b6b53e79e35baf79a4e3
omarabuelkhier <omarabuelkhier@gmail.com>
2024-10-04 12:03:30 +0300

    finish course create form with validation

--- src/app/Course/course-create/course-create.component.ts
+++ src/app/Course/course-create/course-create.component.ts
@@ -9,6 +9,7 @@ import { MatOptionModule } from '@angular/material/core';
 import { ReactiveFormsModule } from '@angular/forms'; // Import ReactiveFormsModule
 import { CoursePlaylistService } from '../../services/course-playlist.service';
 import { GetTeacherService } from '../../services/get-teacher.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-course-create',
@@ -19,6 +20,7 @@ import { GetTeacherService } from '../../services/get-teacher.service';
 })
 export class CourseCreateComponent implements OnInit {
   courseForm!: FormGroup;
+  submitted: boolean = false;
   courses: any;
   instructors: any;
   title=signal("");
@@ -26,25 +28,22 @@ export class CourseCreateComponent implements OnInit {
   thumbnail=signal("");
   instructor_id=signal("");
   playlistId=signal("");
-  instructor_name=signal("");
   constructor(
     private fb: FormBuilder,
-    private coursesService: CoursesService
-    ,private playList: CoursePlaylistService,
-    private teachersService:GetTeacherService
+    private playList: CoursePlaylistService,
+    private teachersService: GetTeacherService,
+    private coursesService: CoursesService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
 
     this.playList.getAllPlayLists().subscribe(courseData =>{
       this.courses = courseData;
-      console.log(this.courses);
     });
     this.teachersService.getAllTeachers().subscribe(teacherData => {
       this.instructors = teacherData;
       this.instructors = this.instructors.data;
-      console.log(this.instructors.data)
-
     });
 
     this.courseForm = this.fb.group({
@@ -53,11 +52,8 @@ export class CourseCreateComponent implements OnInit {
       price: [{ value: '', disabled: false }, [Validators.required, Validators.min(0)]],
       is_free: [false],
       instructor_id: ['', Validators.required],
-      courseSelected: ['', Validators.required],
-      instructorSelected: ['', Validators.required],
-      playlist_id: ['', Validators.required]
-      ,thumbnail:[''],
-      instructor_name:['']
+      playlist_id: ['', Validators.required],
+      thumbnail:[''],
     });
 
     this.courseForm.get('is_free')?.valueChanges.subscribe((isFree) => {
@@ -71,11 +67,18 @@ export class CourseCreateComponent implements OnInit {
   }
 
   createCourse() {
+    this.submitted = true;
+    console.log(this.courseForm.errors)
+
+
+
+
     console.log(this.courseForm.value);
     if (this.courseForm.valid) {
       this.coursesService.createCourse(this.courseForm.value).subscribe(
         (response) => {
           console.log('Course created successfully', response);
+          this.router.navigate(['courses']);
         },
         (error) => {
           console.error('Error creating course', error);
@@ -83,9 +86,10 @@ export class CourseCreateComponent implements OnInit {
       );
     } else {
       console.log('Form is invalid');
+      return;
     }
   }
-  setValue(id: any){
+  setCourseValue(id: any){
     for(let course of this.courses){
       if(course.id === id){
         this.title.set(course.title);
@@ -96,10 +100,20 @@ export class CourseCreateComponent implements OnInit {
           description: this.description(),
           thumbnail:this.thumbnail(),
         });
-        console.log('Course', this.title());
         break;
       };
     }
-    
   }
+  setInstructorValue(id: any){
+    for(let instructor of this.instructors){
+      if(instructor.id === id){
+        this.instructor_id.set(instructor.id);
+        this.courseForm.patchValue({
+          instructor_id: this.instructor_id(),
+        });
+        break;
+      };
+  }
+}
+
 }
