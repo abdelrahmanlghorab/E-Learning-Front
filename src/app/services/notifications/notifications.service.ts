7001f9de73fc090af19476ddbfcd72a54e8c1890
muhammedElnashar <mohamedelnashar01@gmail.com>
2024-10-12 01:49:41 +0300

    Student Notifications

--- src/app/services/notifications/notifications.service.ts
+++ src/app/services/notifications/notifications.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
@@ -7,11 +7,18 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class NotificationsService {
   url = 'http://127.0.0.1:8000/api/user-notifications';
+  Url = 'http://127.0.0.1:8000/api/read-notifications';
   private notification = new BehaviorSubject<any>({});
   userNotifications = this.notification.asObservable();
   http = inject(HttpClient)
   constructor() { }
   getUserNotifications() {
     return this.http.get(this.url);
+  }
+  
+  HttpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
+  markNotificationAsRead(id: any) {
+    
+    return this.http.post(this.Url, id, { headers:this.HttpHeaders });
   }
 }
