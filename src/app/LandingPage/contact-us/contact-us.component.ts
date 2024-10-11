73237edecac6a80c01eafcd2575c49fb7bf96792
abdelrahmanlghorab <abdoelghorab123@gmail.com>
2024-10-11 20:57:39 +0300

    make the user send message to us using email in the contact us

--- src/app/LandingPage/contact-us/contact-us.component.ts
+++ src/app/LandingPage/contact-us/contact-us.component.ts
@@ -1,39 +1,45 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule,FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { MatFormField} from '@angular/material/form-field';
+import { MatFormField } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [MatFormField,MatIconModule,ReactiveFormsModule,CommonModule],
+  imports: [MatFormField, MatIconModule, ReactiveFormsModule, CommonModule],
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.css'
+  styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
   emailForm: FormGroup;
-  constructor(private fb: FormBuilder, private http: HttpClient) {
+
+  constructor(private fb: FormBuilder) {
     this.emailForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]]
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required]]
     });
   }
+
   onSubmit() {
     if (this.emailForm.valid) {
       const formData = this.emailForm.value;
       console.log(formData);
-      this.http.post('http://127.0.0.1:8000/api/subscribe', formData)
-        .subscribe({
-          next: (response) => {
-            // Handle successful response
-            alert('Subscription successful!');
-          },
-          error: (error) => {
-            // Handle error response
-            alert('Subscription failed. Please try again.');
-          }
-        });
+  
+     
+      emailjs.send('service_0d1nvvm', 'template_uzegrj7', {
+        from_name: formData.email, 
+        message: formData.message 
+      }, 'Rgd-zvajBrw3P6u5y')     
+      .then((response: EmailJSResponseStatus) => {
+        console.log(response.text);
+        alert('Message sent successfully!');
+      }, (error) => {
+        console.log(error.text);
+        alert('Failed to send message. Please try again.');
+      });
     }
   }
+  
 }
